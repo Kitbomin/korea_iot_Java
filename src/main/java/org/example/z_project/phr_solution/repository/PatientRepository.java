c6b664efb08e7b4d96a57006f94fe91ce3849c53
Kitbomin <zzxxcc1433@naver.com>
2025-07-25 10:44:15 +0900

    Patient 관리 로직 구현 완료

--- src/main/java/org/example/z_project/phr_solution/repository/PatientRepository.java
+++ src/main/java/org/example/z_project/phr_solution/repository/PatientRepository.java
@@ -18,16 +18,30 @@ public class PatientRepository {
     }
 
     public void save(Patient patient) {
-        
+        // 생성 (Create), 수정(Update) 가 같은 save를 사용
+//        delete(patient); -> 이걸 좀더 고급지게 하면 아래와 같음
+
+        Optional<Patient> existing = findById(patient.getId());
+
+//        existing.ifPresent(value -> this.delete(value));
+        existing.ifPresent(this::delete); //this -> PatientRepository 구성 받아옴 // 전달 받은 환자의 id가 이미 존재하는 경우 -> 삭제
+
+        patients.add(patient);
     }
+
 
     public List<Patient> findAll() {
-        return null;
+        return new ArrayList<>(patients); //새로운 주소값에 담아서 읽기 전용으로 내보냄
     }
 
+
     public Optional<Patient> findById(Long id) {
+        return patients.stream()
+                .filter(patient -> patient.getId().equals(id))
+                .findFirst();
     }
 
     public void delete(Patient patient) {
+        patients.remove(patient);
     }
 }
